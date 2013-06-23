cab1ea4aafc92d2a3c090891b1792c4c16008145
Alexander Luberg <alex@luberg.me>
2013-06-23 19:46:57 +0300

    Black screenshots detection

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ odesk-workdiary-analyzer
 ========================
 
 Analyze your work diary for low activity and "wrong" screenshots
+
+# Dependencies
+# TODO: Would be good to switch to the production python-odesk by oDesk
+* sudo pip install git+git://github.com/solex/python-odesk.git
+* sudo pip install PIL 
+
+
--- analyzer.py
+++ analyzer.py
@@ -1,41 +1,71 @@
 import odesk
 import config
+from PIL import Image
+import io
+
+
+# Detect blackness of the picture to determine
+# if we have a black screenshot
+# 0 to 1, black pictures are those with > 0.9
+def blackness(image_file):
+    image = Image.open(image_file)
+    image = image.convert('L')
+    size = image.size[0] * image.size[1]
+    hist = image.histogram()
+    dark = sum(hist[i] for i in range(10))
+    return dark / float(size)
 
 PUBLIC_KEY = config.public_key or raw_input('Enter public key: ')
 SECRET_KEY = config.secret_key or raw_input('Enter secret key: ')
+USERNAME = config.username or raw_input('Enter your oDesk login: ')
+PASSWORD = config.password or raw_input('Enter your oDesk password: ')
 auth_token = config.auth_token or None
+
+# Add login error handling
+sessionClient = odesk.SessionClient(USERNAME, PASSWORD)
+sessionClient.login()
 
 if auth_token is None:
     client = odesk.Client(PUBLIC_KEY, SECRET_KEY)
     auth_url = client.auth.auth_url()
     print auth_url
+    # TODO: Auto follow the auth link and get the frob
+    #frob_detected = sessionClient.urlopen(auth_url).geturl()
+    #frob_detected = frob_detected.split('?frob=')[1]
+    #print frob_detected
     frob = raw_input("Input frob: ")
     auth_token, user = client.auth.get_token(frob)
 
 
 client = odesk.Client(PUBLIC_KEY, SECRET_KEY, auth_token)
-
+# TODO: Should be parametrized
 start_date = 20130617
-end_date = 20130618
+end_date = 20130624
 
-total_screenshots = 0
-idle_time_seconds = 0
+wrong_screenshots = {}
 
 for tDate in range(start_date, end_date):
-    print tDate
     workdiary = client.team.get_workdiaries(config.team_name,
                                             config.user_id,
                                             date=str(tDate))
-    print workdiary[1]
-    #for snapshot in workdiary[1]:
-        #print "<a href='%s'>%s</a> "%(snapshot[u'screenshot_url'], snapshot[u'activity'])
+    for snapshot in workdiary[1]:
+        if int(snapshot[u'activity']) <= 1:
+            pass
+            # low activity detection
+            # Disabled until API bug is fixed
+            #wrong_screenshots[snapshot[u'screenshot_img_lrg']] =\
+                #int(snapshot[u'time']) - int(snapshot[u'cell_time'])
+        else:
+            # black screenshots detection
+            responce = sessionClient.urlopen(snapshot[u'screenshot_img_lrg'])
+            image = io.BytesIO(responce.read())
+            blackness_level = blackness(image)
+            if blackness_level > 0.9:
+                wrong_screenshots[snapshot[u'screenshot_img_lrg']] =\
+                    int(snapshot[u'time']) - int(snapshot[u'cell_time'])
 
-        #if int(snapshot[u'activity']) <= 1:
-            #print snapshot[u'screenshot_url']
-            #total_screenshots += 1
-            #idle_time_cell = int(snapshot[u'time']) - int(snapshot[u'cell_time'])
-            #idle_time_seconds += idle_time_cell
-
-
-print "Total idle screenshots: %d" % total_screenshots
-print "Idle time (seconds): %d" % idle_time_seconds
+# Recap
+print "Total time taken by wrong screenshots: %f " %\
+    (float(sum(wrong_screenshots.values())) / 3600)
+print "Total number of wrong screenshots: %d" % (len(wrong_screenshots.keys()))
+sessionClient.logout()
